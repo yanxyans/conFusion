3d656377aa9584e5e603aee5345aa6b5cbf3e1be
Jason Yan <fssyan@gmail.com>
2017-12-09 18:55:22 -0500

    http II fix

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
-import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Dish } from '../shared/dish';
 import { baseURL } from '../shared/baseurl';
+
 import { ProcessHttpmsgService } from './process-httpmsg.service';
 
 import 'rxjs/add/operator/delay';
@@ -35,9 +36,9 @@ export class DishService {
                     .catch(error => { return this.processHTTPMsgService.handleError(error); });
   }
 
-  getDishIds(): Observable<number[]> {
+  getDishIds(): Observable<any | number[]> {
     return this.getDishes()
       .map(dishes => { return dishes.map(dish => dish.id) })
-      .catch(error => { return this.processHTTPMsgService.handleError(error); } );
+      .catch(error => { return error; });
   }
 }
