115dad9b3c22eba8b22d52e2de361eac4d0e586b
Jason Yan <fssyan@gmail.com>
2017-12-11 15:53:57 -0500

    e2e test

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,19 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(link: string) {
+    return browser.get(link);
+  }
+  
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
+  }
+  
+  getElement(selector: string) {
+    return element(by.css(selector));
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
   }
 }
